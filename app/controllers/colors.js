1230d668f82946cac77c4c9af2a8b5e1b275f5d6
Sarah Strawn <sarah.c.strawn@gmail.com>
2019-02-22 07:29:28 -0800

    Played with styles

--- app/controllers/colors.js
+++ app/controllers/colors.js
@@ -1,32 +1,19 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+    color: "red",
+    didUpdate() {
+        this.color= "blue";
+        debugger
+    },
+    colorStyle: Ember.computed('color', function() {
+        var color = this.get('color');
+        return new Ember.String.htmlSafe("background-color: " + color);
+    }),
     actions: {
-        getFormData: function (color) {
-            var newCharacter = this.get("newCharacter");
-            var hex = this.get("hex");
-            var url = "https://match-the-magic-server.herokuapp.com/addCharacterByColor";
-            var data = {
-                "color": color.name,
-                "character" : newCharacter,
-                "hex": hex
-            };
-
-            try {
-                $.ajax({
-                type: "POST",
-                url: url,
-                data: data
-                }); 
-            } catch(err) {
-                console.log("having issues");
-            }   
-            window.location.reload(true);
-        },
-
         scrollTo: function(anchor) {
             $('html, body').animate({
-                scrollTop: $("#"+ anchor).offset().top
+                scrollTop: $("#"+ anchor).offset().top - 80
             }, 50)
         },
     }
