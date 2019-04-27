83a21410bff547355d910f7e654dba83b2a37496
Sarah Strawn <sarah.c.strawn@gmail.com>
2019-04-27 14:41:11 -0700

    updated rootURL to hopefully make github pages work

--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   let ENV = {
     modulePrefix: 'db',
     environment,
-    rootURL: '/',
+    rootURL: '/match-the-magic-client',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
